8e204911260b95a37a61816b9654db8dfc526ad7
Grigory Griko <griko1996@gmail.com>
2022-08-30 10:10:23 +0300

    feat: использован контекст в main

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+
+import { CurrentUserContext } from '../contexts/CurrentUserContext.js';
 
 import Header from './Header.js';
 import Main from './Main.js';
 
 import PopupWithForm from './PopupWithForm.js';
 import ImagePopup from './ImagePopup.js';
+import api from '../utils/Api.js';
 
 
 function App() {
@@ -13,6 +16,18 @@ function App() {
   const [isEditAvatarPopupOpen, setIsEditAvatarPopupOpen] = useState(false);
   
   const [selectedCard, setSelectedCard] = useState({});
+
+  const [currentUser, setCurrentUser] = useState({});
+
+  React.useEffect(() => {
+    api.getInitUserData()
+      .then((userData) => {
+        setCurrentUser(userData)
+      })
+      .catch(err => console.log(err));
+
+  }, [])
+  
 
   function handleCardClick(card) {
     setSelectedCard(card);
@@ -39,71 +54,73 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <div className="page">
-          <Header />
-          <Main onEditProfile={handleEditProfileClick} onAddPlace={handleAddPlaceClick} 
-            onEditAvatar={handleEditAvatarClick} onCardClick={handleCardClick} />
-          
-          <PopupWithForm title="Редактировать профиль" name="edit_data" isOpen={isEditProfilePopupOpen} textSubmit="Сохранить" onClose={closeAllPopups}  
-            children= {
-              <>
-                <label className="popup__field">
-                  <input className="popup__name-input input-general-properties" id="nameInput" name="your-name" placeholder="Ваше имя" type="text" defaultValue="" minLength="2" maxLength="40" required />
+    <CurrentUserContext.Provider value={currentUser}>
+      <div className="App">
+        <div className="page">
+            <Header />
+            <Main onEditProfile={handleEditProfileClick} onAddPlace={handleAddPlaceClick} 
+              onEditAvatar={handleEditAvatarClick} onCardClick={handleCardClick} />
+            
+            <PopupWithForm title="Редактировать профиль" name="edit_data" isOpen={isEditProfilePopupOpen} textSubmit="Сохранить" onClose={closeAllPopups}  
+              children= {
+                <>
+                  <label className="popup__field">
+                    <input className="popup__name-input input-general-properties" id="nameInput" name="your-name" placeholder="Ваше имя" type="text" defaultValue="" minLength="2" maxLength="40" required />
 
-                  <span className="popup__input-error nameInput-error">
-                    Ошибка валидации имени
+                    <span className="popup__input-error nameInput-error">
+                      Ошибка валидации имени
+                    </span>
+                  </label>
+
+                  <label className="popup__field">
+                    <input className="popup__job-input input-general-properties" id="jobInput" name="your-job" placeholder="Ваш род деятельности" type="text" defaultValue="" minLength="2" maxLength="200" required />
+
+                    <span className="popup__input-error jobInput-error">
+                      Ошибка валидации рода деятельности
+                    </span>
+                  </label>
+                </>
+              }
+            />
+
+            <PopupWithForm title="Новое место" name="add_card" isOpen={isAddPlacePopupOpen} textSubmit="Создать" onClose={closeAllPopups} 
+              children= {
+                <>
+                  <label className="popup__field">
+                    <input className="popup__name-input input-general-properties" id="nameInputCard" name="card-name" placeholder="Название" type="text" defaultValue="" minLength="2" maxLength="30" required />
+
+                    <span className="popup__input-error nameInputCard-error">
+                      Ошибка валидации названия
+                    </span>
+                  </label>
+
+                  <label className="popup__field">
+                    <input className="popup__job-input input-general-properties" id="linkInput" name="image-link" placeholder="Ссылка на картинку" type="url" defaultValue="" required />
+
+                    <span className="popup__input-error linkInput-error">
+                      Ошибка валидации ссылки на картинку
+                    </span>
+                  </label>
+                </>
+              }
+            />
+
+            <PopupWithForm title="Обновить аватар" name="update_avatar" isOpen={isEditAvatarPopupOpen} textSubmit="Сохранить" onClose={closeAllPopups} 
+              children= {
+                <label className="popup__field">
+                  <input className="popup__name-input input-general-properties" id="linkAvatarInput" name="avatar-link" placeholder="Ссылка на аватар" type="url" defaultValue="" required />
+
+                  <span className="popup__input-error linkAvatarInput-error">
+                    Ошибка валидации ссылки на аватар
                   </span>
                 </label>
+              }
+            />
 
-                <label className="popup__field">
-                  <input className="popup__job-input input-general-properties" id="jobInput" name="your-job" placeholder="Ваш род деятельности" type="text" defaultValue="" minLength="2" maxLength="200" required />
-
-                  <span className="popup__input-error jobInput-error">
-                    Ошибка валидации рода деятельности
-                  </span>
-                </label>
-              </>
-            }
-          />
-
-          <PopupWithForm title="Новое место" name="add_card" isOpen={isAddPlacePopupOpen} textSubmit="Создать" onClose={closeAllPopups} 
-            children= {
-              <>
-                <label className="popup__field">
-                  <input className="popup__name-input input-general-properties" id="nameInputCard" name="card-name" placeholder="Название" type="text" defaultValue="" minLength="2" maxLength="30" required />
-
-                  <span className="popup__input-error nameInputCard-error">
-                    Ошибка валидации названия
-                  </span>
-                </label>
-
-                <label className="popup__field">
-                  <input className="popup__job-input input-general-properties" id="linkInput" name="image-link" placeholder="Ссылка на картинку" type="url" defaultValue="" required />
-
-                  <span className="popup__input-error linkInput-error">
-                    Ошибка валидации ссылки на картинку
-                  </span>
-                </label>
-              </>
-            }
-          />
-
-          <PopupWithForm title="Обновить аватар" name="update_avatar" isOpen={isEditAvatarPopupOpen} textSubmit="Сохранить" onClose={closeAllPopups} 
-            children= {
-              <label className="popup__field">
-                <input className="popup__name-input input-general-properties" id="linkAvatarInput" name="avatar-link" placeholder="Ссылка на аватар" type="url" defaultValue="" required />
-
-                <span className="popup__input-error linkAvatarInput-error">
-                  Ошибка валидации ссылки на аватар
-                </span>
-              </label>
-            }
-          />
-
-          <ImagePopup card={selectedCard} onClose={closeAllPopups} />
-        </div>
-    </div>
+            <ImagePopup card={selectedCard} onClose={closeAllPopups} />
+          </div>
+      </div>
+    </CurrentUserContext.Provider>
   );
 }
 
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 
+import { CurrentUserContext } from '../contexts/CurrentUserContext.js';
+
 import Footer from './Footer.js';
 import api from '../utils/Api.js';
 
@@ -7,23 +9,9 @@ import Card from './Card.js';
 
 
 function Main(props) {
-  const [userName, setUserName] = useState('...Загрузка');
-  const [userDescription, setUserDescription] = useState('...Загрузка');
-  const [userAvatar, setUserAvatar] = useState('');
+  const currentUser = React.useContext(CurrentUserContext);
 
   const [cards, setCards] = useState([]);
-
-
-  React.useEffect(() => {
-    api.getInitUserData()
-      .then((userData) => {
-        setUserName(userData.name);
-        setUserDescription(userData.about);
-        setUserAvatar(userData.avatar);
-      })
-      .catch(err => console.log(err));
-
-  }, [])
 
   React.useEffect(() => {
     api.getInitCards()
@@ -37,14 +25,14 @@ function Main(props) {
   return (
     <main className="content section">
       <section className="profile section-size-full">
-        <button className="profile__button-avatar" style={{ backgroundImage: `url(${userAvatar})` }} type="button" onClick={props.onEditAvatar}></button>
+        <button className="profile__button-avatar" style={{ backgroundImage: `url(${currentUser.avatar})` }} type="button" onClick={props.onEditAvatar}></button>
         <div className="profile__info">
           <div className="profile__wrapper">
-            <h1 className="profile__info-name">{userName}</h1>
+            <h1 className="profile__info-name">{currentUser.name}</h1>
             <button className="profile__button-edit" type="button" onClick={props.onEditProfile}></button>
           </div>
 
-          <p className="profile__info-descript">{userDescription}</p>
+          <p className="profile__info-descript">{currentUser.about}</p>
         </div>
         <button className="profile__button-add" type="button" onClick={props.onAddPlace}></button>
       </section>
